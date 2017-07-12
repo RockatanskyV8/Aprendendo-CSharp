22e7c001b1dd884f236f8a56abf95067adc82223
Lucas Santos Rosa <luccasanttoz@gmail.com>
2017-07-12 13:13:08 -0300

    Outras Aulas

---
+++ CSDrBn_2/Program.cs
@@ -0,0 +1,180 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lucas
+ * Date: 11/07/2017
+ * Time: 12:56
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+using System.Text;
+using System.Globalization;
+
+namespace CSDrBn_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var intVal = 1234;
+			
+			Console.WriteLine("Tipo da intVal : {0}", intVal.GetType());
+			
+			Console.WriteLine();
+			Console.WriteLine("---------------------------ARRAY------------------------------");
+			Console.WriteLine();
+			
+			int [] randNums = new int[3];
+			
+			randNums[0] = 23;
+			
+			Console.WriteLine("Randomicos 0 : {0}", randNums[0]);
+			//esse é bem autoexplicativo
+			string [] pessoas = { "Bob", "Sally", "Sue" };
+			//strings criados em var tem que ter componentes do mesmo tipo
+			var funcionarios = new[] {"Mike", "Paul", "Rick"};
+			//pode-se criar um array de objetos, que cada componente será um objeto
+			object[] randArray = {"Paul", 45, 1.234};
+			
+			Console.WriteLine("randArray 0 : {0}", randArray[0].GetType());
+			Console.WriteLine("Comprimento 0 : {0}", randArray.Length);
+			
+			for(int i = 0; i < randArray.Length; i++)
+            {
+                Console.WriteLine("Posição no vetor {0} : Valor : {1}",
+                i, randArray[i]);
+            }
+			
+			int [] otherRandNums = {1, 4, 9, 2};
+			
+			//função que percorre o array e printa os elementos
+			PrintArray(otherRandNums, "Componente de otherRandNums");
+			
+			Console.WriteLine("4 no index : {0} ", Array.IndexOf(otherRandNums, 4));
+			Array.Sort(otherRandNums);//ordena o Array
+			Console.WriteLine("4 no index : {0} ", Array.IndexOf(otherRandNums, 4));
+			Array.Reverse(otherRandNums);//inverte o Array
+			Console.WriteLine("4 no index : {0} ", Array.IndexOf(otherRandNums, 4));
+			
+			otherRandNums.SetValue(0, 1);
+			
+			//Copiar PARTE do array para outro
+			int[] srcArray = { 1, 2, 3 };
+            int[] destArray = new int[2];
+			int startInd = 0;
+            int length = 2;
+            
+            Array.Copy(srcArray, startInd, destArray, startInd, length);
+            //Array Fonte, Index de inicio, Array Destino, Index de Inicio, comprimento
+            
+            PrintArray(destArray, "Cópia");
+            
+            //Cria um array com o CriateInstance
+            Array outroArray = Array.CreateInstance(typeof(int), 10);
+            
+            //Copia os elementos de srcArray para outroArray a partir do elemento 5
+            srcArray.CopyTo(outroArray, 5);
+            
+            foreach (int m in outroArray)
+            {
+                Console.WriteLine("Copia para : {0} ", m);
+            }
+            
+            //encontra elementos que combinem com a condição fornecida
+            int[] numArray = { 1, 11, 22 };
+            Console.WriteLine("> 10 : {0}", Array.Find(numArray, GT10));
+						
+			Console.WriteLine();
+			Console.WriteLine("---------------ARRAY MULTIDIMENCIONAL-------------------------");
+			Console.WriteLine();
+			
+			String [,] custNames = new String [2,2]{{"Nome 1", "Nome 2"},{"Nome 3", "Nome 4"}};
+			
+			Console.WriteLine("Primeiro Valor : {0}", custNames.GetValue(1,1));
+			
+			for (int i = 0; i < custNames.GetLength(0); i++)
+            {
+				
+                for(int j = 0; j < custNames.GetLength(1); j++)
+                {
+                    Console.Write("{0} ",custNames[i,j]);
+                }
+                Console.WriteLine();
+            }
+			
+			Console.WriteLine();
+			Console.WriteLine("---------------STRINGBUILDER-------------------------");
+			Console.WriteLine();
+			
+// Each time you change a string you are actually creating a new string which is inefficient
+// when you are working with large blocks of text StringBuilders actually change the text
+// rather then make a copy
+			
+			//String builder começãm com 16 bits por default
+			StringBuilder sb = new StringBuilder("Texto Aleatorio");
+			//Criar uma StringBuilder com 256 bits
+			StringBuilder sb2 = new StringBuilder("Algumas coisas bem importantes", 256);
+			
+            Console.WriteLine("Capacidade : {0}", sb2.Capacity);
+ 
+            Console.WriteLine("Comprimento : {0}", sb2.Length);
+ 
+            // Acrescenta algo a uma StringBuilder
+            sb2.AppendLine("\nMais coisas importantes");
+            
+            // Define formato cultural 
+            CultureInfo ptBR = CultureInfo.CreateSpecificCulture("pt-BR");
+ 
+            string funcMes = "Alguém";
+            sb2.AppendFormat(ptBR, "Funcionario do Mês : {0}", funcMes);
+            
+            Console.WriteLine(sb.ToString());//printa sb
+            Console.WriteLine(sb2.ToString());//printa sb2
+            
+            sb2.Replace("coisas", "palavras");//substitui coisas por palavras
+            Console.WriteLine(sb2.ToString());
+            
+            sb2.Clear();//limpa sb2
+ 
+            sb2.Append("Outro texto aleatório");
+            
+            Console.WriteLine(sb2.ToString());
+            
+            Console.WriteLine(sb.Equals(sb2));//compara as duas
+            
+            sb2.Insert(11, " puramente");//acrescenta a partir de determinado indice
+			
+            Console.WriteLine(sb2.ToString());
+            
+            sb2.Remove(11, 7);//remove a partir de determinado indice
+            
+            Console.WriteLine(sb2.ToString());
+            
+			Console.WriteLine();
+			Console.WriteLine("---------------CASTING-------------------------");
+			Console.WriteLine();
+			//quando vc quer converter de um tipo para outro
+			long num1 = 1234;
+			int num2 = (int)num1;
+			
+			Console.WriteLine("Orig : {0} Cast : {1}", num1.GetType(), num2.GetType());
+            
+			Console.Write("Qualquer tecla fecha . . . ");
+			Console.ReadKey(true);
+		}
+		
+		static void PrintArray(int[] intArray, string mess)
+        {
+            foreach (int k in intArray)
+            {
+                Console.WriteLine("{0} : {1} ", mess, k);
+            }
+        }
+		
+		private static bool GT10(int val)
+        {
+            return val > 10;
+        }
+	}
+}
---
+++ CSDrBn_helloworld/Program.cs
@@ -0,0 +1,170 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lucas
+ * Date: 10/07/2017
+ * Time: 19:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Numerics;
+
+namespace CSDrBn_helloworld
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");//printa esta mensagem
+			
+			for(int i = 0; i < args.Length; i++)//conta e printa os argumentos
+			{
+				Console.WriteLine("Arg num {0} = {1}", i , args[i]);
+				//argumentos definidos em CSDrBn_helloworld(botao direito do mouse)>>Proprieties>>Debug
+			}
+			
+			String [] MYargs = Environment.GetCommandLineArgs();
+			
+			Console.WriteLine(String.Join(", " , MYargs));
+			
+			sayHello();
+			
+			Console.WriteLine();
+			Console.WriteLine("------------------Data Tipes--------------------");
+			Console.WriteLine();
+			
+			bool canIVote = true;
+ 
+            // INTEGERS
+            // Integers : 32-bit
+            Console.WriteLine("Maior Integer : {0}", int.MaxValue);
+            Console.WriteLine("Menor Integer : {0}", int.MinValue);
+ 
+            // LONGS
+            // Longs : 64-bit
+            Console.WriteLine("Maior Long : {0}", long.MaxValue);
+            Console.WriteLine("Menor Long : {0}", long.MinValue);
+ 
+            // DECIMALS
+            // Decimals : 128-bit (valores decimais precisos)
+            // Suportam até 28 digitos
+            decimal decPiVal = 3.1415926535897932384626433832M;
+            decimal decBigNum = 3.00000000000000000000000000011M;
+            Console.WriteLine("DEC : PI + bigNum = {0}", decPiVal + decBigNum);
+ 
+            Console.WriteLine("Maior Decimal : {0}", Decimal.MaxValue);
+ 
+            // DOUBLES
+            // Doubles : 64-bit float;
+            Console.WriteLine("Maior Double : {0}", Double.MaxValue.ToString("#"));
+ 
+            // Suporta 14 digitos
+            double dblPiVal = 3.14159265358979;
+            double dblBigNum = 3.00000000000002;
+            Console.WriteLine("DBL : PI + bigNum = {0}", dblPiVal + dblBigNum);
+ 
+            // FLOATS
+            // Floats : 32-bit floats
+            Console.WriteLine("Maior Float : {0}", float.MaxValue.ToString("#"));
+ 
+            // Suporta 6 digitos
+            float fltPiVal = 3.141592F;
+            float fltBigNum = 3.000002F;
+            Console.WriteLine("FLT : PI + bigNum = {0}", fltPiVal + fltBigNum);
+ 
+            // Other Data Types
+            // byte : 8-bit unsigned int 0 to 255
+            // char : 16-bit unicode character
+            // sbyte : 8-bit signed int 128 to 127
+            // short : 16-bit signed int -32,768 to 32,767
+            // uint : 32-bit unsigned int 0 to 4,294,967,295
+            // ulong : 64-bit unsigned int 0 to 18,446,744,073,709,551,615
+            // ushort : 16-bit unsigned int 0 to 65,535
+ 
+            // Da pra converter para string com parse
+            bool boolFromStr = bool.Parse("True");
+            int intFromStr = int.Parse("100");
+            double dblFromStr = double.Parse("1.234");
+			
+			
+			
+						
+			Console.WriteLine();
+			Console.WriteLine("------------------Datatime Timespan--------------------");
+			Console.WriteLine();
+			
+			
+			DateTime algumDia = new DateTime(2010, 07, 21); // define uma data
+			
+			Console.WriteLine("Dia da Semana: {0}", algumDia.DayOfWeek);//retorna o dia da semana
+			
+			algumDia = algumDia.AddDays(4);//adiciona dias(4)
+			Console.WriteLine("Mudança 1: {0}", algumDia);
+			algumDia = algumDia.AddMonths(1);//adiciona meses(1)
+			Console.WriteLine("Mudança 2: {0}", algumDia);
+			algumDia = algumDia.AddYears(1);//adiciona anos(1)
+			Console.WriteLine("Mudança 3: {0}", algumDia);
+			
+			TimeSpan almoco = new TimeSpan(12, 30 , 0);//cria uma hora
+			Console.WriteLine("Horario de Almoço : {0}", almoco);
+			almoco = almoco.Subtract(new TimeSpan(0, 15, 0));//tira 5 min
+			Console.WriteLine("Horario de Almoço : {0}", almoco);
+			almoco = almoco.Add(new TimeSpan(1, 0, 0));//add 1 hora
+			Console.WriteLine("Horario de Almoço : {0}", almoco);
+			
+			
+			Console.WriteLine();
+			Console.WriteLine("----------Big Integer(Precisa do System.Numerics)----------------");
+			Console.WriteLine();
+			
+			BigInteger GrandeInt = BigInteger.Parse("12345123451234512345");//um numero muito grande
+			Console.WriteLine("GrandeInt * 2 = {0}", GrandeInt * 2);//a multiplicação dele por 2
+			
+			Console.WriteLine();
+			Console.WriteLine("-------------------Formatando o OutPut---------------------------");
+			Console.WriteLine();
+			//os numeros são a quantia
+			Console.WriteLine("Grana : {0:c}", 23.455);
+			Console.WriteLine("0s no Inicio : {0:d4}", 23);
+			Console.WriteLine("3 Decimais : {0:f3}", 23.4555);
+			Console.WriteLine("Virgula : {0:n4}", 2300);
+			
+			Console.WriteLine();
+			Console.WriteLine("---------------------------Strings------------------------------");
+			Console.WriteLine();
+			
+			String randString = "Alguma coisa";
+			
+			Console.WriteLine("Comprimento da String : {0}", randString.Length);
+			Console.WriteLine("Contem \"co\" em algum lugar : {0}", randString.Contains("co"));
+			Console.WriteLine("Index de \"Al\" {0}", randString.IndexOf("Al"));
+			Console.WriteLine("Remover Sring : {0}", randString.Remove(0, 5));
+			Console.WriteLine("Inserir String : {0}", randString.Insert(10, "Teste"));
+			Console.WriteLine("Substitui String : {0}", randString.Replace("coisa", "outra coisa"));
+			
+			Console.WriteLine("Compara A com B: {0}", String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
+			Console.WriteLine("A = a: {0}", String.Equals("A", "a", StringComparison.OrdinalIgnoreCase));
+			Console.WriteLine("Pontos Left: {0}", randString.PadLeft(20, '.'));//o numero representa a quantia
+			Console.WriteLine("Pontos Right: {0}", randString.PadRight(20, '.'));//o numero representa a quantia 
+			Console.WriteLine("Trim: {0}", randString.Trim());
+			Console.WriteLine("CAIXA ALTA: {0}", randString.ToUpper());
+			Console.WriteLine("caixa baixa: {0}", randString.ToLower());
+			
+			String novaString = String.Format("{0} saw a {1} {2} in the {3}",
+                							"Paul", "rabbit", "eating", "field");//.format, concatena strings
+			Console.WriteLine(novaString + "\n");//pode-se concatenar strings com um +
+			Console.WriteLine(@"Exactly What I Typed");
+			
+			Console.ReadLine();//Lê uma nova linha para fechar o .exe
+		}
+		
+		private static void sayHello(){
+			string name="";//variavel nome
+			Console.Write("Qual o seu nome: ");//texto de apresentação
+			name = Console.ReadLine();//recebe valor inserido
+			Console.WriteLine("Olá {0}", name);//escreve o nome
+		}
+		
+		
+	}
+}
