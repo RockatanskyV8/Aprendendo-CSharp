7652e26d839debcbf9ff3a6886f5d75e3b37e2e9
Lucas Santos Rosa <luccasanttoz@gmail.com>
2017-07-12 13:08:18 -0300

    Primeiras aulas
    _helloworld: Data Tipes, Datatime Timespan, Big Integer(Precisa do
    System.Numerics), Formatando o OutPut, Strings
    
    _2: ARRAY,ARRAY MULTIDIMENCIONAL, STRINGBUILDER, CASTING

---
+++ CSDrBn/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lucas
+ * Date: 10/07/2017
+ * Time: 19:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace CSDrBn
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
